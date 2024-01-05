49a9a3bba4480dcdb3c13d965dd289abd6155b52
ikergonzalez4 <ikergc1989@gmail.com>
2024-01-05 23:10:10 +0100

    feat: ⭐ add tasks

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { authRequired } from "../middlewares/validateToken.js";
+
+const router = Router() 
+
+router.get('/tasks', authRequired, (req, res) => res.send('tasks'))
+
+export default router;
