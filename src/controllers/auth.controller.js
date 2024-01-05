be4b427683dd81380b1f5fad4271545abf3ef885
ikergonzalez4 <ikergc1989@gmail.com>
2024-01-05 21:45:13 +0100

    new features 😁

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,8 +26,43 @@ export const register = async (req, res) => {
       updatedAt: userSaved.updatedAt,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch ) return res.status (400).json({ message: "Invalid credential" });
+
+    const token = await createAccesToken({ id: userFound._id });
+    
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie('token', "", {
+  expires: new Date(0)
+  })
+  return res.sendStatus(200)
+}
+
+export const profile = (req , res) => {
+  console.log(req.user)
+  res.send('profile')
+}
