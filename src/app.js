0418b8616cebd6bc13ffb33acd9533ed10a6dc0d
ikergonzalez4 <ikergc1989@gmail.com>
2024-01-05 23:13:52 +0100

    feat: 😯 tasks..

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
+
 
 export default app;
