98ff435f1e2a88575acc93d8397ebe0da94315d9
ikergonzalez4 <ikergc1989@gmail.com>
2024-01-06 17:10:44 +0100

    conecting to database 📚

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost/merndb");
-    console.log(">>> DB is connected");
+    console.log(">> DB running");
   } catch (error) {
     console.log(error);
   }
