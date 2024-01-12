c1c7ec10c5be3a72384e1f8541d32bd45af437b0
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:31:48 +0100

    client apis

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,8 @@
-import axios from "axios";
+import axios from "./axios";
 
-const API = "http://localhost:3000/api";
+export const registerRequest = async (user) =>
+  axios.post(`/auth/register`, user);
 
-export const registerRequest = (user) => axios.post(`${API}/register`, user);
+export const loginRequest = async (user) => axios.post(`/auth/login`, user);
+
+export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
