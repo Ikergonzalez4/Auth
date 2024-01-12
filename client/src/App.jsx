da85b58907984379c4a11e29bfa3402d9d68b457
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:33:24 +0100

    client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { Navbar } from "./components/Navbar";
+import { AuthProvider } from "./context/authContext";
+import { ProtectedRoute } from "./routes";
 
+import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
+import { TaskFormPage } from "./pages/TaskFormPage";
+import { LoginPage } from "./pages/LoginPage";
+import { TasksPage } from "./pages/TasksPage";
+import { TaskProvider } from "./context/tasksContext";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Home page</h1>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Tasks page</h1>} />
-          <Route path="/add-task" element={<h1>New task</h1>} />
-          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
-          <Route path="/profile" element={<h1>Profile</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <TaskProvider>
+        <BrowserRouter>
+          <main className="container content-container mx-auto px-10 md:px-0">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/tasks" element={<TasksPage />} />
+                <Route path="/add-task" element={<TaskFormPage />} />
+                <Route path="/tasks/:id" element={<TaskFormPage />} />
+                <Route path="/profile" element={<h1>Profile</h1>} />
+              </Route>
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </TaskProvider>
     </AuthProvider>
   );
 }
