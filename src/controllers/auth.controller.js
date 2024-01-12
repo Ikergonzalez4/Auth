71d6a7bd76b89544789957151951e33c50247987
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:38:55 +0100

    backend controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,32 +1,48 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { createAccesToken } from "../libs/jwt.js";
+import { TOKEN_SECRET } from "../config.js";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
-  const { email, password, username } = req.body;
-
   try {
-    const userFound = await User.findOne({ email });
-    if (userFound) return res.status(400).json(["The email already exists"]);
+    const { username, email, password } = req.body;
 
+    const userFound = await User.findOne({ email });
+
+    if (userFound)
+      return res.status(400).json({
+        message: ["The email is already in use"],
+      });
+
+    // hashing the password
     const passwordHash = await bcrypt.hash(password, 10);
 
+    // creating the user
     const newUser = new User({
       username,
       email,
       password: passwordHash,
     });
 
+    // saving the user in the database
     const userSaved = await newUser.save();
-    const token = await createAccesToken({ id: userSaved._id });
 
-    res.cookie("token", token);
+    // create access token
+    const token = await createAccessToken({
+      id: userSaved._id,
+    });
+
+    res.cookie("token", token, {
+      httpOnly: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "none",
+    });
+
     res.json({
       id: userSaved._id,
       username: userSaved.username,
       email: userSaved.email,
-      createdAt: userSaved.createdAt,
-      updatedAt: userSaved.updatedAt,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -34,49 +50,66 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-
   try {
+    const { email, password } = req.body;
     const userFound = await User.findOne({ email });
-    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    if (!userFound)
+      return res.status(400).json({
+        message: ["The email does not exist"],
+      });
 
     const isMatch = await bcrypt.compare(password, userFound.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid credential" });
+    if (!isMatch) {
+      return res.status(400).json({
+        message: ["The password is incorrect"],
+      });
+    }
 
-    const token = await createAccesToken({ id: userFound._id });
+    const token = await createAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "none",
+    });
+
     res.json({
       id: userFound._id,
       username: userFound.username,
       email: userFound.email,
-      createdAt: userFound.createdAt,
-      updatedAt: userFound.updatedAt,
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };
 
-export const logout = (req, res) => {
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+  if (!token) return res.send(false);
+
+  jwt.verify(token, TOKEN_SECRET, async (error, user) => {
+    if (error) return res.sendStatus(401);
+
+    const userFound = await User.findById(user.id);
+    if (!userFound) return res.sendStatus(401);
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  });
+};
+
+export const logout = async (req, res) => {
   res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
     expires: new Date(0),
   });
   return res.sendStatus(200);
-};
-
-export const profile = async (req, res) => {
-  const userFound = await User.findById(req.user.id);
-
-  if (!userFound) return res.status(400).json({ message: "User not found" });
-
-  return res.jason({
-    id: userFound._id,
-    username: userFound.username,
-    email: userFound.email,
-    createdAt: userFound.createdAt,
-    updatedAt: userFound.updatedAt,
-  });
-  res.send("profile");
 };
