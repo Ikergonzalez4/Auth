97ff7aa6865dc5db7973978091106949d8859c54
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:30:06 +0100

    Delete src/api directory

--- src/api/auth.js
+++
@@ -1,8 +0,0 @@
-import axios from "./axios";
-
-export const registerRequest = async (user) =>
-  axios.post(`/auth/register`, user);
-
-export const loginRequest = async (user) => axios.post(`/auth/login`, user);
-
-export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
