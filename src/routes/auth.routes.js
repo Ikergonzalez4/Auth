c6af492ca1f304206d81f143c48df4fd83d3d168
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:39:57 +0100

    backend routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,22 +1,18 @@
 import { Router } from "express";
 import {
-  register,
   login,
   logout,
-  profile,
+  register,
+  verifyToken,
 } from "../controllers/auth.controller.js";
-import { authRequired } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
-import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
-
 router.post("/login", validateSchema(loginSchema), login);
-
-router.post("/logout", logout);
-
-router.get("/profile", authRequired, profile);
+router.get("/verify", verifyToken);
+router.post("/logout", verifyToken, logout);
 
 export default router;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,30 +1,25 @@
 import { Router } from "express";
-import { authRequired } from "../middlewares/validateToken.js";
 import {
-  getTasks,
-  getTask,
   createTask,
-  updateTask,
   deleteTask,
-} from "../controllers/task.controller.js";
+  getTask,
+  getTasks,
+  updateTask,
+} from "../controllers/tasks.controllers.js";
+import { auth } from "../middlewares/auth.middleware.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
-router.get("/tasks", authRequired, getTasks);
+router.get("/tasks", auth, getTasks);
 
-router.get("/tasks/:id", authRequired, getTask);
+router.post("/tasks", auth, validateSchema(createTaskSchema), createTask);
 
-router.post(
-  "/tasks",
-  authRequired,
-  validateSchema(createTaskSchema),
-  createTask
-);
+router.get("/tasks/:id", auth, getTask);
 
-router.delete("/tasks/:id", authRequired, deleteTask);
+router.put("/tasks/:id", auth, updateTask);
 
-router.put("/tasks/:id", authRequired, updateTask);
+router.delete("/tasks/:id", auth, deleteTask);
 
 export default router;
