cabdac3b73345e4a83223f7deacf7b19cf155342
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-12 15:39:14 +0100

    backend jwt

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,18 +1,11 @@
+import { TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
-import {TOKEN_SECRET} from '../config.js'
 
-export function createAccesToken(payload) {   
-   return new Promise((resolve, reject) => {
-    jwt.sign(
-        payload,
-        TOKEN_SECRET,
-        {
-        expiresIn: "1d",
-        },
-        (err, token) => {
-        if (err) reject(err);
-        resolve(token);
-        }
-    );
-   })
+export async function createAccessToken(payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
 }
